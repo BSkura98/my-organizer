878650e9c29a641d7bb38bcb940f06d5082e0b7c
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2024-11-13 21:21:34 +0100

    Create NotesRepository

--- api/src/main/java/com/my_organizer/api/service/NotesService.java
+++ api/src/main/java/com/my_organizer/api/service/NotesService.java
@@ -1,17 +1,23 @@
 package com.my_organizer.api.service;
 
 import com.my_organizer.api.entity.Note;
+import com.my_organizer.api.repository.NotesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class NotesService {
+
+    private final NotesRepository notesRepository;
+
+    @Autowired
+    public NotesService(NotesRepository notesRepository) {
+        this.notesRepository = notesRepository;
+    }
+
     public List<Note> getNotes() {
-        return List.of(
-                new Note(1L, "This is the first note", LocalDate.now()),
-                new Note(2L, "And this is the second note", LocalDate.now())
-        );
+        return notesRepository.findAll();
     }
 }
