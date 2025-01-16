8580aadeecad17ae1716372239af169e31a1bd4d
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2025-01-16 22:53:39 +0100

    Create initial navigation drawer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,16 @@
+import PersistentDrawer from "./components/PersistentDrawer";
 import Notes from "./pages/Notes";
+import { Tasks } from "./pages/Tasks";
 
 function App() {
   return (
     <>
-      <Notes />
+      <PersistentDrawer
+        menuItems={[
+          { name: "Notes", component: <Notes /> },
+          { name: "Tasks", component: <Tasks /> },
+        ]}
+      />
     </>
   );
 }
